0c8cb7447c9cc5f27bf2bf4656fa018ca60b971a
Luis Sanchez <luis@airbears2-10-142-38-235.airbears2.1918.berkeley.edu>
2018-12-06 00:42:23 -0800

    added unity project w/code and arduino code for wireless/sensor data

---
+++ transmitData/transmitData.ino
@@ -0,0 +1,43 @@
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+float x; 
+float y;
+float z;
+int b;
+String data;
+void setup() {
+  if(!bno.begin()) {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    while(1);
+  }
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+void loop() {
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  x = euler.x();
+  y = euler.y();
+  z = euler.z();
+  data = String(x) + " " + String(y) + " " + String(z);
+  const char* text = data.c_str(); 
+  // will have to change size of multiplier for longer text
+  // remider: try passing in sizeof(*text)
+  radio.write(text, sizeof(text)*10);
+  delay(250);
+}
