a37e5e0991d71c8653365caa9b73195894ce5324
Luis Sanchez <luis@airbears2-10-142-37-191.airbears2.1918.berkeley.edu>
2018-12-08 23:45:18 -0800

    added UDP unity code for kinect data

--- unity_project/Assets/readArdData.cs
+++ unity_project/Assets/readArdData.cs
@@ -4,18 +4,20 @@ using System.Collections.Generic;
 using System;
 using System.Globalization;
 using UnityEngine;
+using System.IO;
 using System.IO.Ports;
 
 
 
-public class readArdData : MonoBehaviour {
+public class readArdData : MonoBehaviour
+{
     // Use this for initialization
 
     public float speed;
-	private float amountToMove;
+    private float amountToMove;
     // make sure to get the right serial port and baud rate for microcontroller
     //
-	SerialPort sp = new SerialPort("/dev/cu.usbmodem14101", 9600);
+    //SerialPort sp = new SerialPort("/dev/cu.usbmodem144401", 9600);
     string data;
     float x;
     float y;
@@ -24,44 +26,47 @@ public class readArdData : MonoBehaviour {
     //like for fingers to a hand
     public GameObject test;
 
-    void Start () {
-		sp.Open();
-		sp.ReadTimeout = 1; 
-		//if timeout set higher unity might freeze while trying to read from the serial port
-		//when the rate is low need a try/catch because unity will check if the port is open
-		//and throw an exeption when it isn't open
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//amountToMove = speed * Time.deltaTime; // makes movement framerate independent
-		if (sp.IsOpen) {
-			try {
-                data = sp.ReadLine();
-                string[] orientation = data.Split(' ');
-                //x = Convert.ToSingle(orientation[0]);
-                //y = Convert.ToSingle(orientation[1]);
-                //z = Convert.ToSingle(orientation[2]);
-                //transform.eulerAngles = new Vector3(-y, x, z);
+    void Start()
+    {
+       //sp.Open();
+        //sp.ReadTimeout = 1;
+        //if timeout set higher unity might freeze while trying to read from the serial port
+        //when the rate is low need a try/catch because unity will check if the port is open
+        //and throw an exeption when it isn't open
+    }
 
-            //using(var reader = new StreamReader(@"~/149/EECS149_Project/KinectCode/CentroidTracking/hand_coordinates.csv")) {
+    // Update is called once per frame
+    void Update()
+    {
+        amountToMove = speed * Time.deltaTime; // makes movement framerate independent
+        using (var reader = new StreamReader(@"hand_coordinates.csv"))
+        {
 
-                //List<string> listA = new List<string>();
-                //List<string> listB = new List<string>();
-                //List<string> listC = new List<string>();
-                //while (!reader.EndOfStream) {
+            List<string> listA = new List<string>();
+            while (!reader.EndOfStream)
+            {
 
-                //    var line = reader.ReadLine();
-                //    var values = line.Split(';');
-                //    listA.Add(values[0]);
-                //    listB.Add(values[1]);
-                //    listC.Add(values[2]);
-                //    }
-                //}
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+                listA.Add(values[0]);
+                Debug.Log(listA[listA.Count - 1]);
             }
-			catch(System.Exception) {
-			}
-		}
+        }
+        //if (sp.IsOpen)
+        //{
+        //    try
+        //    {
+        //        data = sp.ReadLine();
+        //        string[] orientation = data.Split(' ');
+        //        x = Convert.ToSingle(orientation[0]);
+        //        y = Convert.ToSingle(orientation[1]);
+        //        z = Convert.ToSingle(orientation[2]);
+        //        transform.eulerAngles = new Vector3(-y, x, z);
+        //    }
+        //    catch (System.Exception)
+        //    {
+        //    }
+        //}
         //rotates object by repective degrees
         //transform.Rotate(x_degree * Time.deltaTime, y_degree * Time.deltaTime, z_degree * Time.deltaTime, Space.World);
 
@@ -71,22 +76,26 @@ public class readArdData : MonoBehaviour {
 
     // was going to create function to take care of parsing done in update
     // maybe a todo if it gets messy
-    void parseAndAction(string data) {
+    void parseAndAction(string data)
+    {
 
     }
-    
+
     // move object to left or right by amount to move, probably similar logic for up and down
-	void MoveObject(int Direction) {
+    void MoveObject(int Direction)
+    {
 
-		if (Direction == 1) {
-			transform.Translate(Vector3.left * amountToMove, Space.World);
-			//Space.World makes movement relative to world frame
+        if (Direction == 1)
+        {
+            transform.Translate(Vector3.left * amountToMove, Space.World);
+            //Space.World makes movement relative to world frame
 
-		}
-		if (Direction == 2) {
-			transform.Translate(Vector3.right * amountToMove, Space.World);
+        }
+        if (Direction == 2)
+        {
+            transform.Translate(Vector3.right * amountToMove, Space.World);
 
-		}
+        }
 
-	}
+    }
 }
---
+++ unity_project/Assets/readKinect.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class readKinect : MonoBehaviour {
+    String posData;
+    float X;
+    float Y;
+    float Z;
+    private const int listenPort = 9300;
+    //IPAddress comefrom = IPAddress.Parse("127.0.1.1");
+    private void StartListener()
+
+    {
+        UdpClient listener = new UdpClient(listenPort);
+        IPEndPoint groupEP = new IPEndPoint(IPAddress.Loopback, listenPort);
+
+        try
+        {
+            //    Console.WriteLine("Waiting for broadcast");
+                byte[] bytes = listener.Receive(ref groupEP);
+                posData = $" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}");
+                string[] posXYZ = posData.Split(' ');
+                X = Convert.ToSingle(posXYZ[0]);
+                Y = Convert.ToSingle(posXYZ[1]);
+                Z = Convert.ToSingle(posXYZ[2]);
+                transform.position = new Vector3(X, Y, Z);
+            //Console.WriteLine("Received broadcast from {groupEP} :");
+            //Console.WriteLine(" {Encoding.ASCII.GetString(bytes, 0, bytes.Length)}")
+        }
+        catch (SocketException e)
+        {
+            //Console.WriteLine(e);
+        }
+        finally
+        {
+            listener.Close();
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartListener();
+    }
+}
